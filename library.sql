99aba9f26a21c594bd64ac30a2f3815dcef8d3d2
Genaro Almaraz <jg08950@gmail.com>
2020-08-04 15:06:37 -0500

    INSERT, ON DUPLICATE KEY, chunks and subqueries

--- library.sql
+++ library.sql
@@ -62,3 +62,49 @@ CREATE TABLE IF NOT EXISTS operations (
 );
 
 DESC operations;
+
+--INSERT
+INSERT INTO authors(author_id, name, nationality)
+VALUES(NULL, 'Juan Rulfo', 'MEX');
+
+INSERT INTO authors(name, nationality)
+VALUES('Gabriel García Márquez', 'COL');
+
+INSERT INTO authors
+VALUES(NULL, 'Juan Gabriel Vásquez', 'COL');
+
+--Chunks
+INSERT INTO authors(name, nationality)
+VALUES('Isabel Allende', 'CHL'),
+    ('Julio Cortázar', 'ARG'),
+    ('Pablo Neruda', 'CHL');
+
+INSERT INTO authors SET name = 'Carlos Fuentes', nationality = 'MEX';
+
+
+INSERT INTO clients (name, email, birthdate, gender, active)
+VALUES ('Martita Forrestall', 'mforrestall0@weibo.com', '1997-08-08', 'F', 1),
+    ('Car Rolph', 'crolph1@indiegogo.com', '1974-05-06', 'M', 1),
+    ('Giorgi Rolfi', 'grolfi2@scribd.com', '2015-08-13', 'M', 0),
+    ('Marcy Marquis', 'mmarquis3@livejournal.com', '1995-06-26', 'F', 1),
+    ('Innis Charity', 'icharity4@wp.com', '2007-08-07', 'M', 0),
+    ('Leo Romer', 'lromer5@addtoany.com', '1981-04-29', 'M', 1),
+    ('Drake Biaggioli', 'dbiaggioli6@prnewswire.com', '2002-07-07', 'M', 1),
+    ('Wallis Drane', 'wdrane7@omniture.com', '1972-09-15', 'M', 0),
+    ('Jonas Paffitt', 'jpaffitt8@phoca.cz', '2009-05-10', 'M', 1),
+    ('Hartwell Degoe', 'hdegoe9@lycos.com', '1995-03-02', 'M', 1);
+
+--ON DUPLICATE KEY
+--ON DUPLICATE KEY IGNORE ALL -> Disappear all errors
+INSERT INTO clients (name, email, birthdate, gender, active)
+VALUES ('Hartwell Degoe', 'hdegoe9@lycos.com', '1995-03-02', 'M', 0)
+ON DUPLICATE KEY UPDATE active = VALUES(active);
+
+
+SELECT author_id FROM authors WHERE name = 'Pablo Neruda';
+INSERT INTO books(author_id, title) VALUES(6, 'Cien sonetos de amor');
+
+--Nested queries -> Subquery
+INSERT INTO books(author_id, title) VALUES(
+    (SELECT author_id FROM authors WHERE name = 'Pablo Neruda' LIMIT 1),
+    'Confieso que he vivido');
